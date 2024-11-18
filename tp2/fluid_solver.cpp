ab8b82771028bf08e7fa94e3af5f257483a2e754
Gustominox <agstcmps.111@gmail.com>
2024-11-18 13:13:00 +0000

    Add private omp call

--- tp2/fluid_solver.cpp
+++ tp2/fluid_solver.cpp
@@ -82,7 +82,7 @@ void lin_solve(int M, int N, int O, int b, float *x, float *x0, float a, float c
     max_c = 0.0f;
 
     // Red points update
-#pragma omp parallel for collapse(2) reduction(max : max_c)
+#pragma omp parallel for collapse(2) reduction(max : max_c) private(old_x, change)
     for (int i = 1; i <= M; i++)
     {
       for (int j = 1; j <= N; j++)
@@ -106,7 +106,7 @@ void lin_solve(int M, int N, int O, int b, float *x, float *x0, float a, float c
 #pragma omp barrier
 
     // Black points update
-#pragma omp parallel for collapse(2) reduction(max : max_c)
+#pragma omp parallel for collapse(2) reduction(max : max_c) private(old_x, change)
     for (int i = 1; i <= M; i++)
     {
       for (int j = 1; j <= N; j++)
