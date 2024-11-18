a41c615a929a8fdfb5f839e075f5e5e4a3929ca0
Gustominox <agstcmps.111@gmail.com>
2024-11-18 14:20:32 +0000

    k assignment before loop

--- tp2/fluid_solver.cpp
+++ tp2/fluid_solver.cpp
@@ -90,7 +90,9 @@ void lin_solve(int M, int N, int O, int b, float *x, float *x0, float a, float c
       for (int j = 1; j <= N; j++)
       {
 #pragma omp simd
-        for (int k = 1 + (i + j) % 2; k <= O; k += 2)
+        int k = 1 + (i + j) % 2;
+#pragma omp parallel for
+        for (k; k <= O; k += 2)
         {
           old_x = x[IX(i, j, k)];
           x[IX(i, j, k)] = (x0[IX(i, j, k)] +
@@ -114,7 +116,9 @@ void lin_solve(int M, int N, int O, int b, float *x, float *x0, float a, float c
       for (int j = 1; j <= N; j++)
       {
 #pragma omp simd
-        for (int k = 1 + (i + j + 1) % 2; k <= O; k += 2)
+        int k = 1 + (i + j) % 2;
+#pragma omp parallel for
+        for (k; k <= O; k += 2)
         {
           old_x = x[IX(i, j, k)];
           x[IX(i, j, k)] = (x0[IX(i, j, k)] +
